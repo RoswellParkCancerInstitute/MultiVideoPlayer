b7d60425f2df98f57404819496e94e2acca4684c
Sridhar Mane <sridhars4s@gmail.com>
2017-05-31 10:14:54 -0400

    Cleaned up imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
 
-import { AppComponent } from './app.component';
-import { VideoControlsComponent } from './components/video-controls/video-controls.component';
-import { VideoElementsComponent } from './components/video-elements/video-elements.component';
-import { StateManagerService } from './services/state-manager.service';
-import { PreferencesService } from './services/preferences.service';
-import { VideoElementComponent } from './components/video-element/video-element.component';
-import { SecondsToTimePipe } from './pipes/seconds-to-time.pipe';
-import { RoundOffPipe } from './pipes/round-off.pipe';
-// import { VideoTimeDisplayComponent } from './components/video-controls/video-time-display/video-time-display.component';
-import { DialogComponent } from './components/dialog/dialog.component';
-import { PreferencesComponent } from './components/preferences/preferences.component';
-import { KeyboardShortcutsComponent } from './components/keyboard-shortcuts/keyboard-shortcuts.component';
-// import { CopyToClipboardButtonComponent } from './components/copy-to-clipboard-button/copy-to-clipboard-button.component';
-
+import {AppComponent} from './app.component';
+import {
+  VideoControlsComponent,
+  VideoElementsComponent,
+  VideoElementComponent,
+  KeyboardShortcutsComponent,
+  PreferencesComponent,
+  DialogComponent
+} from './components';
+import {LoggerService, PreferencesService, StateManagerService, SnackbarService} from './services';
+import {SecondsToTimePipe, RoundOffPipe} from './pipes';
 
 @NgModule({
   declarations: [
@@ -45,13 +42,11 @@ import { KeyboardShortcutsComponent } from './components/keyboard-shortcuts/keyb
     BrowserAnimationsModule
   ],
   providers: [
-    StateManagerService,
-    PreferencesService
+    LoggerService, StateManagerService, PreferencesService, SnackbarService
   ],
-  entryComponents:[
-    PreferencesComponent,
-    KeyboardShortcutsComponent
+  entryComponents: [
+    PreferencesComponent, KeyboardShortcutsComponent
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
