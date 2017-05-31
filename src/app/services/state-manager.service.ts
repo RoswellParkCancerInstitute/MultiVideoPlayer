52a13f74c4d74bb81e98f89dd413a2e7ff291e29
Sridhar Mane <sridhars4s@gmail.com>
2017-05-31 10:21:30 -0400

    Correctly Copy time according to prefs
    Correctly Copy time according to prefs
    Use constants from utils filder for strings

--- src/app/services/state-manager.service.ts
+++ src/app/services/state-manager.service.ts
@@ -1,9 +1,12 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import {Injectable, EventEmitter} from '@angular/core';
 // import { Subject } from 'rxjs/Subject';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { PreferencesService, IPreferences } from './preferences.service';
-
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {PREFERENCES, TIME_DISPLAY_FORMATS} from '../utils/constants';
+import {PreferencesService, IPreferences} from './preferences.service';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+import {SnackbarService} from './snackbar.service';
+import {RoundOffPipe, SecondsToTimePipe} from '../pipes';
+import {DatePipe} from '@angular/common';
 
 @Injectable()
 export class StateManagerService {
@@ -12,63 +15,50 @@ export class StateManagerService {
    * Variables
    */
 
-  videos = new BehaviorSubject<any>([]);
+  videos = new BehaviorSubject < any > ([]);
 
-  playerStatus = new BehaviorSubject<IPlayerStatus>({ status: PLAYER_STATUS.PAUSED });
+  playerStatus = new BehaviorSubject < IPlayerStatus > ({status: PLAYER_STATUS.PAUSED});
   playerPlayStatus = PLAYER_STATUS.PAUSED;
-  isPlayerEnabled = new BehaviorSubject<boolean>(false);
-  muteStatus = new EventEmitter<MUTE_STATUS>();
-  timeUpdate = new BehaviorSubject<number>(0);
-  volumeUpdate = new EventEmitter<PLAYER_STATUS>();
-  duration = new EventEmitter<PLAYER_STATUS>();
+  isPlayerEnabled = new BehaviorSubject < boolean > (false);
+  muteStatus = new EventEmitter < MUTE_STATUS > ();
+  timeUpdate = new BehaviorSubject < number > (0);
+  volumeUpdate = new EventEmitter < PLAYER_STATUS > ();
+  duration = new EventEmitter < PLAYER_STATUS > ();
   maxDuration = 0;
   masterIndex = 0; // index of master video for which time updates will be sent
 
-
   private acceptedFileTypes = ['video/mp4', 'video/ogv', 'video/mpeg'];
 
-  isMediaReady: EventEmitter<boolean>;
+  isMediaReady : EventEmitter < boolean >;
 
   preferencesChanged = this.preferencesService.preferences;
 
-  constructor(
-    private sanitizer: DomSanitizer,
-    private preferencesService: PreferencesService
-  ) {
+  constructor(private sanitizer : DomSanitizer, private preferencesService : PreferencesService, private snackbar : SnackbarService) {
     this.sanitizer = sanitizer;
 
     this.isMediaReady = new EventEmitter();
 
     // Set plyer states based on new preferences
-    // this.preferencesChanged.subscribe(preferences => {
-    //   // Make live changes to player only if it is currently enabled
-    //   if (this.isPlayerEnabled.getValue()) {
-    //     for (const key in preferences) {
-    //       if (key) {
-    //         switch (key) {
-    //           case 'playbackRate':
-    //               this.
-    //             break;
-
-    //           default:
-    //             break;
-    //         }
-    //       }
-    //     }
-
-    //   }
-    // });
+    // this.preferencesChanged.subscribe(preferences => {   // Make live changes to
+    // player only if it is currently enabled   if (this.isPlayerEnabled.getValue())
+    // {     for (const key in preferences) {  if (key) {         switch (key) {
+    //       case 'playbackRate':   this.             break;     default:
+    //  break;         }       }    }   } });
   }
 
   play() {
     console.log('Playing');
     this.playerPlayStatus = PLAYER_STATUS.PLAYING;
-    this.playerStatus.next({ status: PLAYER_STATUS.PLAYING });
+    this
+      .playerStatus
+      .next({status: PLAYER_STATUS.PLAYING});
   }
   pause() {
     console.log('Pausing');
     this.playerPlayStatus = PLAYER_STATUS.PAUSED;
-    this.playerStatus.next({ status: PLAYER_STATUS.PAUSED });
+    this
+      .playerStatus
+      .next({status: PLAYER_STATUS.PAUSED});
   }
   togglePlayPause() {
     switch (this.playerPlayStatus) {
@@ -83,27 +73,41 @@ export class StateManagerService {
     }
   }
   seekRight() {
-    const currentTime = this.timeUpdate.getValue();
-    const seekValue = this.preferencesChanged.getValue().seekStep;
+    const currentTime = this
+      .timeUpdate
+      .getValue();
+    const seekValue = this
+      .preferencesChanged
+      .getValue()
+      .seekStep;
     const newTime = currentTime + seekValue;
     this.seekTo(newTime);
   }
   seekLeft() {
-    const currentTime = this.timeUpdate.getValue();
-    const seekValue = this.preferencesChanged.getValue().seekStep;
+    const currentTime = this
+      .timeUpdate
+      .getValue();
+    const seekValue = this
+      .preferencesChanged
+      .getValue()
+      .seekStep;
     let newTime = currentTime - seekValue;
     if (newTime < 0) {
       newTime = 0;
     }
     this.seekTo(newTime);
   }
-  seekTo(seconds: number): void {
+  seekTo(seconds : number) : void {
     console.log('Seeking to:', seconds);
-    this.playerStatus.next({ status: PLAYER_STATUS.SEEK_TO, value: seconds });
+    this
+      .playerStatus
+      .next({status: PLAYER_STATUS.SEEK_TO, value: seconds});
   }
 
   increaseVolume() {
-    let volume = this.preferencesChanged.getValue()['volume'];
+    let volume = this
+      .preferencesChanged
+      .getValue()['volume'];
     volume += 10;
 
     if (volume > 100) {
@@ -114,7 +118,9 @@ export class StateManagerService {
   }
 
   decreaseVolume() {
-    let volume = this.preferencesChanged.getValue()['volume'];
+    let volume = this
+      .preferencesChanged
+      .getValue()['volume'];
     volume -= 10;
 
     if (volume < 0) {
@@ -123,92 +129,152 @@ export class StateManagerService {
 
     this.setVolume(volume);
   }
-  setVolume(volume: number) {
+  setVolume(volume : number) {
     console.log('Setting Volume to:', volume);
-    this.preferencesService.setPref('volume', volume);
-    this.playerStatus.next({ status: PLAYER_STATUS.VOLUME, value: volume });
+    this
+      .preferencesService
+      .setPref('volume', volume);
+    this
+      .playerStatus
+      .next({status: PLAYER_STATUS.VOLUME, value: volume});
   }
 
-
-  setPlaybackSpeed(speed: number) {
+  setPlaybackSpeed(speed : number) {
     console.log('Setting Playback Speed to:', speed);
-    this.preferencesService.setPref('playbackSpeed', speed);
-    this.playerStatus.next({ status: PLAYER_STATUS.PLAYBACK_SPEED, value: speed });
+    this
+      .preferencesService
+      .setPref('playbackSpeed', speed);
+    this
+      .playerStatus
+      .next({status: PLAYER_STATUS.PLAYBACK_SPEED, value: speed});
   }
 
   copyToClipboard() {
+    const seconds = this.timeUpdate.getValue();
     const body = document.getElementsByTagName('body');
+
+    // create a temp texarea element so we can use JS to emulate a select and copy event.
     const textArea = document.createElement('textarea');
-    document.body.appendChild(textArea);
-    textArea.value = '' + this.timeUpdate.getValue();
+    document
+      .body
+      .appendChild(textArea);
+
+    textArea.value = this.formatSecondsToPreferredFormat(seconds, this.preferencesService.getPref(PREFERENCES.timeDisplayFormat));
+
     textArea.select();
 
     try {
       const successful = document.execCommand('copy');
-      const msg = successful ? 'successful' : 'unsuccessful';
+      const msg = successful
+        ? 'successful'
+        : 'unsuccessful';
       console.log('Copy command was ' + msg);
+      this
+        .snackbar
+        .show('Time Copied to clipboard');
+
+      // check if pause player on copy pref is set
+      if (this.preferencesService.getPref(PREFERENCES.pauseOnCopy)) {
+        this.pause();
+      }
+
     } catch (err) {
-      console.log('Oops, unable to cut');
+      this
+        .snackbar
+        .show(`Error Copying Time: ${JSON.stringify(err)}`);
+      console.log('Oops, unable to copy');
     }
-    document.body.removeChild(textArea);
+
+    // cleanup the dom, remove the temp element
+    document
+      .body
+      .removeChild(textArea);
+  }
+
+  formatSecondsToPreferredFormat(seconds : number, format = TIME_DISPLAY_FORMATS.hmmss) : string {
+    if(format === TIME_DISPLAY_FORMATS.hmmss) {
+      const timePipe = new SecondsToTimePipe(); // converts seconds to a datetime object
+      const datePipe = new DatePipe('en'); // converts datetime object to required format
+      let formattedTime = '';
+      formattedTime = timePipe.transform(seconds);
+      formattedTime = datePipe.transform(formattedTime, TIME_DISPLAY_FORMATS.hmmss);
+      return formattedTime;
+    }
+    return seconds + '';
   }
 
   /**
  * Adds the video to the player
  */
-  registerVideoElements(files: Array<File>): void {
+  registerVideoElements(files : Array < File >) : void {
     // First filter out the file types that are not accepted
-    const videos = files.filter(file =>
-      this.acceptedFileTypes.indexOf(file.type) ? false : true
-    ).map(file => {
+    const videos = files.filter(file => this.acceptedFileTypes.indexOf(file.type)
+      ? false
+      : true).map(file => {
       return {
-        sources: [{
-          src: this.getObjectURL(file),
-          type: file.type
-        }]
+        sources: [
+          {
+            src: this.getObjectURL(file),
+            type: file.type
+          }
+        ]
       };
     });
     console.log(`Adding ${videos.length} valid videos to player`);
 
-    this.videos.next(videos);
+    this
+      .videos
+      .next(videos);
   }
   /**
    * Each video elemnt calls this funciton, we only need to store the max duration
    * @param duration
    */
-  setDuration(duration: number, videoIndex: number) {
+  setDuration(duration : number, videoIndex : number) {
     if (!this.maxDuration) {
       this.maxDuration = duration;
       this.masterIndex = videoIndex;
-      this.duration.next(duration);
+      this
+        .duration
+        .next(duration);
     } else {
       if (duration > this.maxDuration) {
         this.maxDuration = duration;
         this.masterIndex = videoIndex;
-        this.duration.next(duration);
+        this
+          .duration
+          .next(duration);
       }
     }
   }
 
-  setTimeUpdate(seconds: number, videoIndex: number) {
+  setTimeUpdate(seconds : number, videoIndex : number) {
     if (videoIndex === this.masterIndex) {
-      this.timeUpdate.next(Math.round(seconds));
+      this
+        .timeUpdate
+        .next(Math.round(seconds));
       // console.log(seconds);
     }
   }
 
-  videosLoaded(videosLoaded: any) {
+  videosLoaded(videosLoaded : any) {
     console.log('videosLoaded', videosLoaded);
 
     if (Object.keys(videosLoaded).length === this.videos.getValue().length) {
-      this.isPlayerEnabled.next(true);
+      this
+        .isPlayerEnabled
+        .next(true);
     } else {
-      this.isPlayerEnabled.next(false);
+      this
+        .isPlayerEnabled
+        .next(false);
     }
   }
-  addVideoSource(file): void {
+  addVideoSource(file) : void {
     // get current length so we can add ids to newly added files
-    const videos = this.videos.getValue();
+    const videos = this
+      .videos
+      .getValue();
     const currentFileIndex = videos.length + 1;
     const video = {
       sources: [
@@ -220,26 +286,32 @@ export class StateManagerService {
       ]
     };
   }
-  clearVideoAtIndex(videoIndex: number) {
-    const newVideosList = this.videos.getValue().splice(videoIndex, 1);
-    this.videos.next(newVideosList);
+  clearVideoAtIndex(videoIndex : number) {
+    const newVideosList = this
+      .videos
+      .getValue()
+      .splice(videoIndex, 1);
+    this
+      .videos
+      .next(newVideosList);
   }
 
   /**
   * Gets the blob url of the file so we can handle it without knowing the actual location
   * */
-  getObjectURL(file): SafeUrl {
-    return this.sanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(file));
+  getObjectURL(file) : SafeUrl {
+    return this
+      .sanitizer
+      .bypassSecurityTrustUrl(window.URL.createObjectURL(file));
   }
-
 
 }
 export interface IVideoSource {
-  src: string;
-  type: 'video/ogv' | 'video/webm' | 'video/mp4';
+  src : string;
+  type : 'video/ogv' | 'video/webm' | 'video/mp4';
 }
 export interface IVideo {
-  sources: IVideoSource[];
+  sources : IVideoSource[];
 }
 export enum PLAYER_STATUS {
   PLAYING,
@@ -254,7 +326,7 @@ export enum MUTE_STATUS {
   UNMUTED
 }
 export interface IPlayerStatus {
-  status: PLAYER_STATUS;
+  status : PLAYER_STATUS;
   value?: number;
 }
 
@@ -264,60 +336,49 @@ const test = [
       {
         src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.ogv',
         type: 'video/ogv'
-      },
-      {
+      }, {
         src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm',
         type: 'video/webm'
-      },
-      {
+      }, {
         src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
         type: 'video/mp4'
       }
     ]
-  },
-  {
+  }, {
     sources: [
       {
         src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.ogv',
         type: 'video/ogv'
-      },
-      {
+      }, {
         src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm',
         type: 'video/webm'
-      },
-      {
+      }, {
         src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
         type: 'video/mp4'
       }
     ]
-  },
-  {
+  }, {
     sources: [
       {
         src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.ogv',
         type: 'video/ogv'
-      },
-      {
+      }, {
         src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm',
         type: 'video/webm'
-      },
-      {
+      }, {
         src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
         type: 'video/mp4'
       }
     ]
-  },
-  {
+  }, {
     sources: [
       {
         src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.ogv',
         type: 'video/ogv'
-      },
-      {
+      }, {
         src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm',
         type: 'video/webm'
-      },
-      {
+      }, {
         src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
         type: 'video/mp4'
       }
